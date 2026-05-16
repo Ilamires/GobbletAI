a07caa07fb1002b2659eabdf4c9b95156e6679b2
Ilamires <v.shamraev1@yandex.ru>
2026-05-16 18:20:07 +0300

    Add game figure buttons on game page

---
+++ Gobblet/GameSettings.cs
@@ -0,0 +1,20 @@
+namespace Gobblet;
+
+public class GameSettings
+{
+    public bool PlayerGoFirst { get; set; }
+
+    public uint Turn { get; private set; }
+    public byte CurrentPlayer { get => (byte)(Turn % 2); }
+    public bool IsPlayerTurn
+    {
+        get => PlayerGoFirst && CurrentPlayer == 1 ||
+               !PlayerGoFirst && CurrentPlayer == 0;
+    }
+    
+    public GameSettings(bool playerGoFirst = true)
+    {
+        PlayerGoFirst = playerGoFirst;
+        Turn = 1;
+    }
+}
--- Gobblet/Pages/GamePage.xaml.cs
+++ Gobblet/Pages/GamePage.xaml.cs
@@ -7,16 +7,32 @@ namespace Gobblet.Pages;
 
 public partial class GamePage : Page
 {
+    
+    GameSettings settings;
     public byte FieldSize { get; } = 3;
     public byte FieldSectionWidth { get; } = 90;
 
     GameFieldButton[][] FieldButtons;
+    GameButton[][] FirstPlayerButtons;
+    GameButton[][] SecondPlayerButtons;
+    GameButton[][][] PlayerButtons;
     public GamePage(bool playerGoFirst)
     {
         InitializeComponent();
+        
+        settings = new GameSettings(playerGoFirst);
 
         setFieldButtons();
         placeFieldButtons();
+
+        FirstPlayerButtons = new GameButton[FieldSize][];
+        SecondPlayerButtons = new GameButton[FieldSize][];
+        PlayerButtons = new GameButton[][][] { FirstPlayerButtons, SecondPlayerButtons };
+
+        setPlayerButtons(0);
+        placePlayerButtons(0);
+        setPlayerButtons(1);
+        placePlayerButtons(1);
     }
 
     public void setFieldButtons()
@@ -53,7 +69,60 @@ public partial class GamePage : Page
         }
     }
     
+    public void setPlayerButtons(int playerNumber)
+    {
+        for (int i = 0; i < PlayerButtons[playerNumber].Length; ++i)
+        {
+            PlayerButtons[playerNumber][i] = new GameButton[2];
+            for (int j = 0; j < PlayerButtons[playerNumber][i].Length; ++j)
+            {
+                PlayerButtons[playerNumber][i][j] =
+                    new GameButton(j + 1, (GameButton.GameButtonSizes)(i + 1));
+                int buttonSize = 50 + (int)(PlayerButtons[playerNumber][i][j].Size - 1) * 15;
+                PlayerButtons[playerNumber][i][j].Width = buttonSize;
+                PlayerButtons[playerNumber][i][j].Height = buttonSize;
+                PlayerButtons[playerNumber][i][j].ButtonColor = playerNumber == 0 ? Brushes.Blue : Brushes.Orange;
+                PlayerButtons[playerNumber][i][j].IsEnabled = (settings.PlayerGoFirst && playerNumber == 0 ||
+                                                              !settings.PlayerGoFirst && playerNumber == 1);
+                PlayerButtons[playerNumber][i][j].Click += PlayerButton_Click;
+            }
+        }
+    }
+    
+    public void placePlayerButtons(int playerNumber)
+    {
+        for (int row = 0; row < PlayerButtons[playerNumber].Length; ++row)
+        {
+            for (int col = 0; col < PlayerButtons[playerNumber][row].Length; ++col)
+            {
+                if (settings.PlayerGoFirst && playerNumber == 0 || !settings.PlayerGoFirst && playerNumber == 1)
+                {
+                    int realRow = PlayerButtons[playerNumber].Length - 1 - row;
+                    Grid.SetRow(PlayerButtons[playerNumber][realRow][col], row);
+                    Grid.SetColumn(PlayerButtons[playerNumber][realRow][col], col + 1);
+                    InnerGridBottomLeft.Children.Add(
+                        PlayerButtons[playerNumber][realRow][col]);
+                }
+                else
+                {
+                    Grid.SetRow(PlayerButtons[playerNumber][row][col], row);
+                    Grid.SetColumn(PlayerButtons[playerNumber][row][col], col);
+                    InnerGridTopRight.Children.Add(PlayerButtons[playerNumber][row][col]);
+                }
+            }
+        }
+    }
+    
     public void TakeTurn_Click(object sender, EventArgs e)
+    {
+        TakeTurn();
+    }
+    
+    public void PlayerButton_Click(object sender, EventArgs e)
+    {
+        
+    }
+    public void TakeTurn()
     {
         
     }
